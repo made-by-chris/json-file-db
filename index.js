485ea35dfc77236ac351a1268563f340c0c62a34
z-memory <dont@forgetmy.email>
2023-06-14 16:14:30 +0200

    set up instructions

---
+++ index.js
@@ -0,0 +1,23 @@
+// JSON-FILE-DB
+// import fs
+
+// DO THIS
+// startup procedure
+// (fs) read the file if exists, if not create (db.json)
+// load file into js array (parse)
+
+// USE THESE
+// const db = []
+// function insert()
+// function select()
+// function update()
+// function remove()
+
+// whenever user writes data in any way, "persist" that write operation to disk.
+// stringify fs
+
+// EXAMPLES OF USAGE
+insert({ name: "sarah", location: "moscow" });
+// select()
+// update(3, { name: "bob312312" });
+// remove(4)
